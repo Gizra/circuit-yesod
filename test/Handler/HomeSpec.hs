edd130cf9fe386de2ef40daea646ba98116655b4
Amitaibu <amitai@gizra.com>
2016-09-05 13:36:06 +0300

    Homepage is 404

--- test/Handler/HomeSpec.hs
+++ test/Handler/HomeSpec.hs
@@ -8,13 +8,13 @@ spec = withApp $ do
     describe "Homepage" $ do
       it "loads the index and checks it looks right" $ do
           get HomeR
-          statusIs 200
+          statusIs 403
 
       -- This is a simple example of using a database access in a test.  The
       -- test will succeed for a fresh scaffolded site with an empty database,
       -- but will fail on an existing database with a non-empty user table.
       it "leaves the user table empty" $ do
           get HomeR
-          statusIs 200
+          statusIs 403
           users <- runDB $ selectList ([] :: [Filter User]) []
           assertEq "user table empty" 0 $ length users
