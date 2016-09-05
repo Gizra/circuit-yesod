b74ebafb3572d57c9d202c946aa8c7fa27d9a884
Amitai Burstein <amitai@gizra.com>
2016-09-05 23:49:44 +0300

    Show user's bids

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -1,10 +1,14 @@
 module Handler.Profile where
 
 import Import
+import           Database.Persist.Sql (fromSqlKey)
 
 getProfileR :: Handler Html
 getProfileR = do
-    (_, user) <- requireAuthPair
+    (userId, user) <- requireAuthPair
+
+    bids <- runDB $ selectList [BidBidder ==. userId] [Desc BidId]
+
     defaultLayout $ do
         setTitle . toHtml $ userIdent user `mappend` "'s User page"
         $(widgetFile "profile")
