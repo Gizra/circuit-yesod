1a24155f3632f92380e7ca761c5b9183a59a708d
Amitaibu <amitai@gizra.com>
2016-09-05 11:56:23 +0300

    Remove scaffolded models

--- Foundation.hs
+++ Foundation.hs
@@ -135,7 +135,6 @@ instance Yesod App where
 
     -- Routes not requiring authentication.
     isAuthorized (AuthR _) _ = return Authorized
-    isAuthorized CommentR _ = return Authorized
     isAuthorized HomeR _ = return Authorized
     isAuthorized FaviconR _ = return Authorized
     isAuthorized RobotsR _ = return Authorized
