a13066def20e023696c57d4c5c064d1ded0c554a
Amitai Burstein <amitai@gizra.com>
2017-04-08 20:56:35 +0300

    add test

--- test/Handler/RestfulBidSpec.hs
+++ test/Handler/RestfulBidSpec.hs
@@ -9,15 +9,22 @@ spec = withApp $ do
 
     describe "REStful Bid page" $ do
         it "Checks a valid bid ID" $ do
+            currentTime <- liftIO getCurrentTime
+
             userEntity <- createUser "bar"
-            authenticateAs userEntity
             let (Entity uid _) = userEntity
+
+            _ <- runDB . insert $ AccessToken currentTime uid "someRandomToken"
 
             (Entity saleId _)  <-  createSale uid "sale1"
             (Entity itemId _)  <- createItem uid saleId "item1" 0 10 100
             (Entity bidId _) <- createBid uid itemId 150
 
-            get $ RestfulBidR bidId
+            request $ do
+                setMethod "GET"
+                setUrl $ RestfulBidR bidId
+                addGetParam "access_token" "someRandomToken"
+
             statusIs 200
 
         it "should show bidder property to bid owner" $ do
