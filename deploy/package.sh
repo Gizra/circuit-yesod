67c90c34f4ed21c65bb6e74b278570d471037ef8
Aron Novak <aron@gizra.com>
2017-04-13 16:25:13 +0200

    package.sh: first working version of the script

--- deploy/package.sh
+++ deploy/package.sh
@@ -1,7 +1,11 @@
-# @todo: Manipulate config
+#!/bin/bash
+# Prepares the material to build a Docker image
+set -o xtrace
+
+cp deploy/deploy-settings.yml config/settings.yml
 stack build
 TARGET=deploy/dist
 mkdir "$TARGET"
-cp `find -executable -type f | grep circuit-yesod$ | head -n1` "$TARGET"
+cp "$(find -executable -type f | grep circuit-yesod$ | head -n1)" "$TARGET"
 cp -r static $TARGET
 cp -r config $TARGET
