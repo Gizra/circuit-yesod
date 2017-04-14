388633f5b943a1e37235b487a0d8c7d47ab10776
Aron Novak <aron@gizra.com>
2017-04-14 09:45:01 +0200

    package.sh: check repo root location

--- deploy/package.sh
+++ deploy/package.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # Prepares the material to build a Docker image
-set -o xtrace
 
+if [[ ! -d config ]]; then
+  echo "Please switch to repository root directory first"
+  exit 1
+fi
+
+set -o xtrace
+rm -rf .stack-work/dist
 cp deploy/deploy-settings.yml config/settings.yml
 stack build
 TARGET=deploy/dist
-mkdir "$TARGET"
+rm -rf $TARGET
+mkdir -p "$TARGET"
 cp "$(find -executable -type f | grep circuit-yesod$ | head -n1)" "$TARGET"
 cp -r static $TARGET
 cp -r config $TARGET
