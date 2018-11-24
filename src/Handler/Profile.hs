f307e9afab1deb6ff07da73e2fa23b655a0626f0
amitaibu <amitai@gizra.com>
2018-11-24 18:37:12 +0200

    Add Lucid

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -1,15 +1,24 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+
 module Handler.Profile where
 
 import Import
 
-getProfileR :: Handler Html
+import Lucid
+import Text.Blaze.Html
+import YesodLucid
+
+getProfileR :: Handler Import.Html
 getProfileR = do
-    (_, user) <- requireAuthPair
-    defaultLayout $ do
-        setTitle . toHtml $ userIdent user <> "'s User page"
-        $(widgetFile "profile")
+  (_, user) <- requireAuthPair
+  lucidHtml <-
+    lucid $ \urlRender ->
+      p_
+        (do a_ [href_ (urlRender ProfileR)] (Lucid.toHtml $ userIdent user)
+            div_ "Some text")
+  defaultLayout $ do
+    setTitle . Import.toHtml $ userIdent user <> "'s User page"
+    toWidget . preEscapedToHtml . renderText $ lucidHtml
